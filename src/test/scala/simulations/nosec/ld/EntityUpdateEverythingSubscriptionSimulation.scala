345b8a8354959beaf3c48cd1a0c8a6c985daa8d4
Stefan Wiedemann <wistefan@googlemail.com>
2021-02-02 10:53:45 +0100

    add everything subscription test

---
+++ src/test/scala/simulations/nosec/ld/EntityUpdateEverythingSubscriptionSimulation.scala
@@ -0,0 +1,44 @@
+package simulations.nosec.ld
+
+import java.util.UUID
+import java.util.concurrent.TimeUnit
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+import scalaj.http.Http
+
+class EntityUpdateEverythingSubscriptionSimulation extends EntityUpdateSimulation {
+
+  override def beforeScenario(): Unit = {
+    val subscription = getEverythingSubscriptionAction(testConfig.notificationServerUrl)
+    val response = Http(baseUrl + "subscriptions").header("Content-Type", "application/ld+json").postData(subscription).timeout(1000, 6000).asString
+    if (response.code > 299 || response.code < 200) {
+      println("Was not able to setup the Subscription. Response: " + response + ", Subscription:  " + subscription)
+    }
+  }
+
+  override def afterScenario(): Unit = {
+    Http(baseUrl + "subscriptions/urn:ngsi-ld:Subscription:everything").header("Content-Type", "application/ld+json").method("DELETE").timeout(1000, 6000).asString
+  }
+
+  override def getScenario(): ScenarioBuilder = {
+    val notificationServerUrl = testConfig.notificationServerUrl
+    scenario("Parallel entity updates with subscription")
+      .exec(session => session.set("entityId", UUID.randomUUID()))
+      .exec(
+        createTimedEntityAction()
+      )
+      .pause(updateDelay.toString, TimeUnit.SECONDS)
+      .repeat(numberOfUpdatesToSimulate) {
+        exec(
+          updateTimedEntityAction()
+        )
+          // wait for the new values to be available
+          .pause(updateDelay.toString, TimeUnit.SECONDS)
+      }
+      // cleanup
+      .exec(
+        deleteTimedEntityAction()
+      )
+  }
+}
