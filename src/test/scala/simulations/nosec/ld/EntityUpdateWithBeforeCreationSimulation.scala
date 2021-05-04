d815c7ca66436249556f93650e7c00b4742e6646
Stefan Wiedemann <wistefan@googlemail.com>
2021-05-04 07:41:37 +0200

    create before update

---
+++ src/test/scala/simulations/nosec/ld/EntityUpdateWithBeforeCreationSimulation.scala
@@ -0,0 +1,58 @@
+package simulations.nosec.ld
+
+import java.util.UUID
+import java.util.concurrent.TimeUnit
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+import scalaj.http.Http
+import simulations.FiwareLDBaseSimulation
+
+class EntityUpdateWithBeforeCreationSimulation extends FiwareLDBaseSimulation {
+
+  final val  entityIdPrefix:String = UUID.randomUUID().toString;
+  final val entityIdList: List[UUID] = Stream.tabulate(testConfig.numEntities)(n => UUID.fromString(entityIdPrefix + "-" + n)).toList
+
+  override def getParallelRuns(): Int = {
+    testConfig.numEntities
+  }
+
+  override def beforeScenario(): Unit = {
+
+    for (a <- 0 to testConfig.numEntities - 1) {
+
+      val response = Http(baseUrl + "entityOperations/create").header("Content-Type", "application/ld+json").postData(getUpdateBody(a * 100, (a + 1) * 100, entityIdList)).timeout(10000, 60000).asString
+
+      if (response.code != 200) {
+        throw new RuntimeException("Was not able to prefill the database. Response: " + response)
+      }
+    }
+  }
+
+  override def getScenario(): ScenarioBuilder = {
+
+    val idFeeder = Iterator.tabulate(testConfig.numEntities)(n => Map("entityId" -> (entityIdPrefix + "-" + n)))
+    scenario("Parallel entity updates")
+      .feed(idFeeder)
+      .exec(
+        createEntityAction()
+      )
+      .pause(testConfig.updateDelay.toString, TimeUnit.SECONDS)
+      .repeat(testConfig.numUpdates) {
+        exec(
+          updateEntityAction("temperature")
+        )
+          // wait for the new values to be available
+          .pause(testConfig.updateDelay.toString, TimeUnit.SECONDS)
+          .exec(
+            updateEntityAction("humidity")
+          )
+          // wait for the new values to be available
+          .pause(testConfig.updateDelay.toString, TimeUnit.SECONDS)
+      }
+      // cleanup
+      .exec(
+        deleteEntityAction()
+      )
+  }
+}
