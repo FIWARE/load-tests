ff38d269f582a9481e711e8ef83d2edbe3439217
Stefan Wiedemann <wistefan@googlemail.com>
2021-09-21 08:38:41 +0200

    fix response code check

--- src/test/scala/simulations/FiwareLDBaseSimulation.scala
+++ src/test/scala/simulations/FiwareLDBaseSimulation.scala
@@ -40,7 +40,7 @@ abstract class FiwareLDBaseSimulation extends Simulation {
         println("Create batch from " + a * 100 + " to " + (a + 1) * 100)
         val response = Http(baseUrl + "entityOperations/create").header("Content-Type", "application/ld+json").postData(getUpdateBody(a * 100, (a + 1) * 100, prefillEnitiyIdList)).timeout(10000, 60000).asString
 
-        if (response.code > 200 && response.code <= 299) {
+        if (response.code > 200 && response.code >= 299) {
           throw new RuntimeException("Was not able to prefill the database. Response: " + response)
         }
       }
